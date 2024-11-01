07a7423df713aa38199342cb6de6549f1c42b7ec
CAAAndrade <159110347+CAAAndrade@users.noreply.github.com>
2024-11-01 06:52:54 +0000

    People repository implementation

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,8 +1,10 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from src.models.sqlite.repositories.pets_repository import PetsRepository
+from src.models.sqlite.repositories.people_repository import PeopleRepository
 
 db_connection_handler.connect_to_db()
+
 
 @pytest.mark.skip(reason="interação com o BD")
 def test_list_pets():
@@ -11,8 +13,28 @@ def test_list_pets():
     print()
     print(response)
 
+
+@pytest.mark.skip(reason="interação com o BD")
 def test_delete_pet():
     name = "belinha"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pet(name)
-    
+
+
+@pytest.mark.skip(reason="interação com o BD")
+def test_insert_person():
+    first_name = "John"
+    last_name = "Doe"
+    age = 30
+    pet_id = 1
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+
+@pytest.mark.skip(reason="interação com o BD")
+def test_get_person():
+    person_id = 1
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print()
+    print(response)
