5361026b2d4045027c30f810d5243d8b6c0b951e
CAAAndrade <159110347+CAAAndrade@users.noreply.github.com>
2024-11-05 17:32:40 +0000

    ending person_finder_controller + tests

--- src/controllers/pet_creator_controller.py
+++
@@ -1,6 +0,0 @@
-from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
-
-
-class PetCreatorController:
-    def __init__(self, pets_repository: PetsRepositoryInterface) -> None:
-        self.__pets_repository = pets_repository
